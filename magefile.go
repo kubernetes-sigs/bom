bb0c973d5bea8f0a8d9482e080c28f9cddd368fd
cpanato <ctadeu@gmail.com>
2022-03-02 11:39:30 +0100

    update golangci-lint to v1.44.2
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- magefile.go
+++ magefile.go
@@ -90,7 +90,7 @@ func Verify() error {
 	}
 
 	fmt.Println("Running golangci-lint...")
-	if err := mage.RunGolangCILint("v1.44.0", false); err != nil {
+	if err := mage.RunGolangCILint("v1.44.2", false); err != nil {
 		return err
 	}
 
